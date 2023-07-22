452b163aebfea7da88c7c24ccaa56be9c18db25b
Muwonge Nicholus <nicholusmuwonge@gmail.com>
2023-07-22 19:35:10 +0300

    Update the simulation random number generation based on MLCG.

--- main.c
+++ main.c
@@ -3,33 +3,30 @@
 #include "monte_carlo.h"
 
 int main() {
-    int N = 500; // Number of random points
-    int num_threads_list[] = {1, 2, 4, 8}; // List of number of threads to test
+    int global_number_of_simulation_points = 500;
+    int num_threads_list[] = {1, 2, 4, 8};
 
-    printf("Function: y = x^2\n");
+    printf("Function: y = x ** x\n");
 
-    // Sequential code performance
     printf("\nSequential Code:\n");
     double start_time = omp_get_wtime();
-    double result_seq = monte_carlo_integration(N, 1); // Single thread for sequential code
+    double result_seq = monte_carlo_integration(global_number_of_simulation_points, 1); 
     double end_time = omp_get_wtime();
     printf("Approximate integral value (Sequential): %lf\n", result_seq);
     printf("Execution Time (Sequential): %lf seconds\n", end_time - start_time);
 
-    // Parallel code performance
     printf("\nParallel Code:\n");
     for (int i = 0; i < sizeof(num_threads_list) / sizeof(num_threads_list[0]); i++) {
         int num_threads = num_threads_list[i];
-        printf("Number of Threads: %d\n", num_threads);
+        printf("Number of threads loaded: %d\n", num_threads);
 
         start_time = omp_get_wtime();
-        double result_par = monte_carlo_integration(N, num_threads);
+        double result_par = monte_carlo_integration(global_number_of_simulation_points, num_threads);
         end_time = omp_get_wtime();
 
         printf("Approximate integral value (Parallel, %d Threads): %lf\n", num_threads, result_par);
         printf("Execution Time (Parallel, %d Threads): %lf seconds\n", num_threads, end_time - start_time);
 
-        // Performance comparison
         double speedup = (end_time - start_time) / (end_time - start_time);
         double efficiency = speedup / num_threads;
         printf("Speedup: %lf\n", speedup);
@@ -37,13 +34,11 @@ int main() {
         printf("\n");
     }
 
-    // Now, let's use the new function y = 2x for the Monte Carlo simulation
-    printf("\nFunction: y = 2x\n");
+    printf("\nfunction: y = 2x\n");
 
-    // Sequential code performance
     printf("\nSequential Code:\n");
     start_time = omp_get_wtime();
-    result_seq = monte_carlo_integration(N, 1); // Single thread for sequential code
+    result_seq = monte_carlo_integration(global_number_of_simulation_points, 1);
     end_time = omp_get_wtime();
     printf("Approximate integral value (Sequential): %lf\n", result_seq);
     printf("Execution Time (Sequential): %lf seconds\n", end_time - start_time);
@@ -52,10 +47,10 @@ int main() {
     printf("\nParallel Code:\n");
     for (int i = 0; i < sizeof(num_threads_list) / sizeof(num_threads_list[0]); i++) {
         int num_threads = num_threads_list[i];
-        printf("Number of Threads: %d\n", num_threads);
+        printf("Number of threads the task is distributed amongst: %d\n", num_threads);
 
         start_time = omp_get_wtime();
-        double result_par = monte_carlo_integration(N, num_threads);
+        double result_par = monte_carlo_integration(global_number_of_simulation_points, num_threads);
         end_time = omp_get_wtime();
 
         printf("Approximate integral value (Parallel, %d Threads): %lf\n", num_threads, result_par);
@@ -70,17 +65,16 @@ int main() {
     }
 
 
-    printf("\nApproximating π using Monte Carlo simulation:\n");
-    int N_pi = 1000000; // Number of random points for π approximation
-    int num_threads_pi = 4; // Number of threads for π approximation
+    printf("\nApproximating Pi(π) value using Monte Carlo simulation:\n");
+    int no_of_simulation_points = 10000;
+    int num_threads_pi = 8;
 
     double start_time_pi = omp_get_wtime();
-    double approximate_pi = monte_carlo_pi(N_pi, num_threads_pi); // Call the function with correct declaration
+    double cumulative_sum_from_all_threads = monte_carlo_pi(no_of_simulation_points, num_threads_pi);
     double end_time_pi = omp_get_wtime();
 
-    printf("Approximate value of π: %lf\n", approximate_pi);
-    printf("Execution Time (π approximation): %lf seconds\n", end_time_pi - start_time_pi);
-
+    printf("Simulation generated Pi(π) value of: %lf\n", cumulative_sum_from_all_threads);
+    printf("Time taken to generate Pi(π) value: %lf seconds\n", end_time_pi - start_time_pi);
 
     return 0;
 }
--- monte_carlo.c
+++ monte_carlo.c
@@ -11,7 +11,7 @@ double f(double x) {
 }
 
 // Function to check if a point (x, y) lies inside the unit circle
-int is_inside_circle(double x, double y) {
+int within_circle_radius(double x, double y) {
     return (x * x + y * y) <= 1.0;
 }
 
@@ -20,37 +20,44 @@ int is_inside_region(double x, double y) {
     return (y <= f(x));
 }
 
-// Monte Carlo simulation for numerical integration of the function y = f(x) over the interval [0, 10]
-double monte_carlo_integration(int N, int num_threads) {
-    int count_inside = 0;
-    double domain_min = 0.0;
-    double domain_max = 10.0;
-    double range_min = 0.0;
-    double range_max = 100.0; // For the new function y = 2x, range is [0, 100]
+// Multiplicative Linear Congruential Generator (MLCG)
+double mlcg(double seed, double lambda, double P) {
+    return fmod(lambda * seed, P);
+}
 
-    srand(time(NULL)); // Seed the random number generator
+// Monte Carlo simulation for numerical integration of the function y = f(x) over the interval [0, 10]
+double monte_carlo_integration(int number_of_simulations, int num_threads) {
+    int inside_shaded_region_count = 0;
+    double domain_min_x = 0.0;
+    double domain_max_x = 10.0;
+    double range_min_y = 0.0;
+    double range_max_y = 10.0;
+
+    int seed = time(NULL); // Initialize the seed for MLCG
 
     // Set the number of threads for the parallel region
     omp_set_num_threads(num_threads);
 
-    // Parallel loop to generate N random points and count the points inside the shaded region
-    #pragma omp parallel for reduction(+:count_inside)
-    for (int i = 0; i < N; i++) {
-        // Generate random (x, y) coordinates within the specified domain and range
-        double x = domain_min + (double)rand() / RAND_MAX * (domain_max - domain_min);
-        double y = range_min + (double)rand() / RAND_MAX * (range_max - range_min);
+    #pragma omp parallel for reduction(+:inside_shaded_region_count)
+    for (int i = 0; i < number_of_simulations; i++) {
+        // Generate random (x, y) coordinates within the specified domain and range using MLCG
+        seed = mlcg(seed, 3, 2147483647); // Using λ = 3 and P(Modulus) = 2^31 - 1
+        double x = domain_min_x + ((double)seed / 2147483647.0) * (domain_max_x - domain_min_x);
+        
+        seed = mlcg(seed, 3, 2147483647);
+        double y = range_min_y + ((double)seed / 2147483647.0) * (range_max_y - range_min_y);
 
         // Check if the point (x, y) lies inside the shaded region under the curve y = f(x)
         if (is_inside_region(x, y)) {
-            count_inside++; // Increment the count of points inside the region
+            inside_shaded_region_count++; // Increment the count of points inside the region
         }
     }
 
     // Calculate the ratio of points inside the region to total points generated
-    double ratio_inside = (double)count_inside / N;
+    double ratio_inside = (double)inside_shaded_region_count / number_of_simulations;
 
     // Calculate the total area of the region (domain x range)
-    double total_area = (domain_max - domain_min) * (range_max - range_min);
+    double total_area = (domain_max_x - domain_min_x) * (range_max_y - range_min_y);
 
     // Approximate the integral of the function y = f(x) using the Monte Carlo simulation
     double approximate_integral = ratio_inside * total_area;
@@ -58,35 +65,36 @@ double monte_carlo_integration(int N, int num_threads) {
 }
 
 // Monte Carlo simulation to approximate the value of π by generating random points inside a unit square
-double monte_carlo_pi(int N, int num_threads) {
+double monte_carlo_pi(int number_of_simulations, int num_threads) {
     int count_inside_circle = 0;
-    double domain_min = -1.0;
-    double domain_max = 1.0;
-    double range_min = -1.0;
-    double range_max = 1.0;
+    double domain_min_x = -1.0;
+    double domain_max_x = 1.0;
+    double range_min_y = -1.0;
+    double range_max_y = 1.0;
 
-    srand(time(NULL)); // Seed the random number generator
+    double seed = time(NULL); // Initialize the seed for MLCG
 
-    // Set the number of threads for the parallel region
     omp_set_num_threads(num_threads);
 
-    // Parallel loop to generate N random points and count the points inside the unit circle
     #pragma omp parallel for reduction(+:count_inside_circle)
-    for (int i = 0; i < N; i++) {
-        // Generate random (x, y) coordinates within the specified domain and range
-        double x = domain_min + (double)rand() / RAND_MAX * (domain_max - domain_min);
-        double y = range_min + (double)rand() / RAND_MAX * (range_max - range_min);
+    for (int i = 0; i < number_of_simulations; i++) {
+        // Generate random (x, y) coordinates within the specified domain and range using MLCG
+        seed = mlcg(seed, 3, 2147483647); // Using λ = 3 and P = 2^31 - 1
+        double x = domain_min_x + (seed / 2147483647.0) * (domain_max_x - domain_min_x);
+        
+        seed = mlcg(seed, 3, 2147483647);
+        double y = range_min_y + (seed / 2147483647.0) * (range_max_y - range_min_y);
 
         // Check if the point (x, y) lies inside the unit circle
-        if (is_inside_circle(x, y)) {
-            count_inside_circle++; // Increment the count of points inside the circle
+        if (within_circle_radius(x, y)) {
+            count_inside_circle++; 
         }
     }
 
-    // Calculate the ratio of points inside the unit circle to total points generated
-    double ratio_inside_circle = (double)count_inside_circle / N;
+    // Calculate the ratio of points inside the unit circle in relation to the total points generated
+    double ratio_inside_circle = (double)count_inside_circle / number_of_simulations;
 
-    // Approximate the value of π using the Monte Carlo simulation
-    double approximate_pi = 4.0 * ratio_inside_circle;
-    return approximate_pi;
+    // Approximate the value of Pi(π) using the Monte Carlo simulation
+    double generated_approximated_pi = 4.0 * ratio_inside_circle;
+    return generated_approximated_pi;
 }
